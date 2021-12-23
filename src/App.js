967b42e03a9157d25feb95879df723a311d69357
Developer Junaid <qjunaid623@gmail.com>
2021-12-23 11:09:45 +0500

    collection card done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import "./App.css";
 
 // Components
 import Header from "./components/Header/Header";
+import CollectionCard from "./components/Punklist/CollectionCard/CollectionCard";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name="Bandana Punk"
+        traits={[{ value: 7 }]}
+        image="https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu"
+      />
     </div>
   );
 }
